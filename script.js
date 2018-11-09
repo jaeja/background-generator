ddbf607d7bef94b150861ff4b32feb1ad723bb77
jaeja <akobije@yahoo.com>
2018-11-09 20:26:36 +0000

    adding project files

---
+++ script.js
@@ -0,0 +1,40 @@
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+
+function inputLength() {
+	return input.value.length;
+}
+
+function createListElement() {
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	ul.appendChild(li);
+	input.value = "";
+}
+
+function addListAfterClick() {
+	if (inputLength() > 0) {
+		createListElement();
+	}
+}
+
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+		createListElement();
+	}
+}
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
+
+var listToggle=document.querySelectorAll("li");
+
+var array = [].slice.call(listToggle);
+
+function toggy(){
+	 listToggle.classList.toggle("done")
+}
+
+console.log(array[2])
